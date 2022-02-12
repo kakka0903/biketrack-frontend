1a3db98dc901ed21cdba70f6ff1199ecab04c7e1
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-02-12 23:33:03 +0100

    update deviceStatCard to look better when wider

--- src/components/deviceStatCard.vue
+++ src/components/deviceStatCard.vue
@@ -16,13 +16,11 @@ const props = defineProps({
 </script>
 
 <template>
-    <card>
-        <div class="flex items-center justify-center flex-grow w-1/4 ">
-            <div class="flex items-center justify-center w-8 h-8 rounded-full bg-sky-100 text-sky-400">
-                <component class="w-6 h-6" :is="icon"/>
-            </div>
+    <card class="gap-5">
+        <div class="flex items-center justify-center w-8 h-8 rounded-full bg-sky-100 text-sky-400">
+            <component class="w-6 h-6" :is="icon"/>
         </div>
-        <div class="flex flex-col flex-grow mr-5">
+        <div class="flex-col">
             <p class="text-xl">{{value}}</p>
             <p class="text-xs text-gray-500">{{subtext}}</p>
         </div>
